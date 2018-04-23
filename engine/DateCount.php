396c03d056397612fb6525810c8b016dd4a3fc78
Buza Péter <szotyi41@yahoo.com>
2018-04-23 19:38:44 +0200

    Post and comment works

---
+++ engine/DateCount.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Szotyi Lehet
+ * Date: 2018. 04. 22.
+ * Time: 1:18
+ */
+
+namespace engine;
+
+
+class DateCount
+{
+
+}
---
+++ template/home.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Szotyi Lehet
+ * Date: 2018. 04. 21.
+ * Time: 22:55
+ */
